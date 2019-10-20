1243a13847f6291348f37c0f5cb423692e620a05
mattyen07 <mgyen@shaw.ca>
2019-10-20 14:30:30 -0700

    added constructor

--- src/main/java/cpen221/mp2/graph/Graph.java
+++ src/main/java/cpen221/mp2/graph/Graph.java
@@ -9,7 +9,111 @@ import java.util.*;
  */
 public class Graph<V extends Vertex, E extends Edge<V>> implements ImGraph<V, E>, IGraph<V, E> {
 
-    // TODO: Implement this type
+    private Map<V, Set<E>> graph = new HashMap<>();
+
+    public Graph() {
+
+    }
+
+
+    @Override
+    public boolean addVertex(V v) {
+        Set<E> set = new HashSet<>();
+
+        this.graph.put(v, set);
+
+        return this.graph.containsKey(v);
+    }
+
+    @Override
+    public boolean vertex(V v) {
+        return this.graph.containsKey(v);
+    }
+
+    @Override
+    public boolean addEdge(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean edge(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean edge(V v1, V v2) {
+        return false;
+    }
+
+    @Override
+    public int edgeLength(V v1, V v2) {
+        return 0;
+    }
+
+    @Override
+    public int edgeLengthSum() {
+        return 0;
+    }
+
+    @Override
+    public boolean remove(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(V v) {
+        return false;
+    }
+
+    @Override
+    public Set<V> allVertices() {
+        return null;
+    }
+
+    @Override
+    public Set<E> allEdges(V v) {
+        return null;
+    }
+
+    @Override
+    public Set<E> allEdges() {
+        return null;
+    }
+
+    @Override
+    public Map<V, E> getNeighbours(V v) {
+        return null;
+    }
+
+    @Override
+    public List<V> shortestPath(V source, V sink) {
+        return null;
+    }
+
+    @Override
+    public List<E> minimumSpanningTree() {
+        return null;
+    }
+
+    @Override
+    public int pathLength(List<V> path) {
+        return 0;
+    }
+
+    @Override
+    public Set<V> search(V v, int range) {
+        return null;
+    }
+
+    @Override
+    public int diameter() {
+        return 0;
+    }
+
+    @Override
+    public E getEdge(V v1, V v2) {
+        return null;
+    }
 
     //// add all new code above this line ////
 

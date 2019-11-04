4de8eb0b5f197ff18a6b8723cdffce9d7494e4c8
mattyen07 <mgyen@shaw.ca>
2019-11-04 15:43:17 -0800

    fixed bug in hunt

--- src/main/java/cpen221/mp2/spaceship/MillenniumFalcon.java
+++ src/main/java/cpen221/mp2/spaceship/MillenniumFalcon.java
@@ -10,12 +10,10 @@ import cpen221.mp2.models.Planet;
 import cpen221.mp2.models.PlanetStatus;
 import cpen221.mp2.util.Heap;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -32,21 +30,28 @@ public class MillenniumFalcon implements Spaceship {
         Map<Integer, Integer> parentList = new HashMap<>();
         int currID = state.currentID();
         int nextMove = state.currentID();
-        // set earth as current id, traverse down a path, if we hit a dead end, move back up the path until we have a new path
 
-        parentList.put(currID, currID);
+        // set earth as current id, traverse down a path, if we hit a dead end,
+        // move back up the path until we have a new path
 
+        parentList.put(currID, currID); //puts earth into parent list
+
+        //while we are not on kamino, continue to go down a path
         while (!state.onKamino()) {
             PlanetStatus[] neighbours = state.neighbors();
             double maxSignal = 0;
 
+            //for each planet in the neighbour array, find the planet with the
+            // strongest signal that hasn't been visited
             for (PlanetStatus planet : neighbours) {
-                if (planet.signal() > maxSignal && !visited.contains(planet.id())) {
+                if (planet.signal() >= maxSignal && !visited.contains(planet.id())) {
                     maxSignal = planet.signal();
                     nextMove = planet.id();
                 }
             }
 
+            //if our visited doesn't have the nextMove, then we add to the visited list and move
+            //otherwise we go back on our path to find a planet that has unvisited neighbours
             if (!visited.contains(nextMove)) {
                 visited.add(nextMove);
                 state.moveTo(nextMove);
